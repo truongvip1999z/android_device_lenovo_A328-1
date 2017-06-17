8843fcc021d147e8989fe8536fb2f1e70df6ec73
Vojtěch Hejsek <hejsekvojtech@gmail.com>
2017-06-17 20:03:02 +0200

    A328: correct patch path

--- patches/apply.sh
+++ patches/apply.sh
@@ -2,30 +2,30 @@
 
  cd ../../../..
  cd system/core
- patch -p1 < ../../device/lenovo/A536/patches/system_core.patch
+ patch -p1 < ../../device/lenovo/A328/patches/system_core.patch
  cd ..
  cd bt
- patch -p1 < ../../device/lenovo/A536/patches/system_bt.patch
+ patch -p1 < ../../device/lenovo/A328/patches/system_bt.patch
  cd ..
  cd netd
- patch -p1 < ../../device/lenovo/A536/patches/system_netd.patch
+ patch -p1 < ../../device/lenovo/328/patches/system_netd.patch
  cd ../..
  cd hardware/libhardware
- patch -p1 < ../../device/lenovo/A536/patches/hardware_libhardware.patch
+ patch -p1 < ../../device/lenovo/A328/patches/hardware_libhardware.patch
  cd ../..
  cd frameworks/av
- patch -p1 < ../../device/lenovo/A536/patches/frameworks_av.patch
+ patch -p1 < ../../device/lenovo/A328/patches/frameworks_av.patch
  cd ..
  cd base
- patch -p1 < ../../device/lenovo/A536/patches/frameworks_base.patch
+ patch -p1 < ../../device/lenovo/A328/patches/frameworks_base.patch
  cd ..
  cd native
- patch -p1 < ../../device/lenovo/A536/patches/frameworks_native.patch
+ patch -p1 < ../../device/lenovo/A328/patches/frameworks_native.patch
  cd ..
  cd opt/telephony
- patch -p1 < ../../../device/lenovo/A536/patches/frameworks_opt_telephony.patch
+ patch -p1 < ../../../device/lenovo/A328/patches/frameworks_opt_telephony.patch
  cd ../../..
  cd external/sepolicy
- patch -p1 < ../../device/lenovo/A536/patches/external_sepolicy.patch
+ patch -p1 < ../../device/lenovo/A328/patches/external_sepolicy.patch
  cd ../..
  echo Successfuly patched! Go ahead and start building!
