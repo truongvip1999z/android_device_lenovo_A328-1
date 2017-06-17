f89d855292ead946d64aa97f15619a9f58cb65f7
hejsekvojtech <hejsekvojtech@gmail.com>
2017-06-17 19:27:06 +0200

    Initial upload

---
+++ README.md
@@ -0,0 +1,21 @@
+Lenovo A328
+==========
+
+Lenovo A328 device (codenamed _"A328"_) is low-range smartphone from Lenovo.
+
+Lenovo A328 was announced in 2014, Septeber.
+
+Basic   | Spec Sheet
+-------:|:-------------------------
+CPU     | 1.3GHz Quad-Core MT6582M
+GPU     | Mali-400MP2
+Memory  | 1GB RAM
+Shipped Android Version | 4.4.2
+Storage | 4GB
+Battery | 2000 mAh
+Display | 4.5" 480 x 854 px
+Camera  | 5MPx, LED Flash
+
+![Lenovo A328](https://im9.cz/iR/importprodukt-orig/9fa/9fa1d62bd06b22229cf3efc9a1440d29.jpg "Lenovo A328")
+
+This branch is for building LineageOS 13.0 (or Android Marshmallow 6.0.1 AOSP based ROMs) ROMs.
---
+++ patches/apply.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+ cd ../../../..
+ cd system/core
+ patch -p1 < ../../device/lenovo/A536/patches/system_core.patch
+ cd ..
+ cd bt
+ patch -p1 < ../../device/lenovo/A536/patches/system_bt.patch
+ cd ..
+ cd netd
+ patch -p1 < ../../device/lenovo/A536/patches/system_netd.patch
+ cd ../..
+ cd hardware/libhardware
+ patch -p1 < ../../device/lenovo/A536/patches/hardware_libhardware.patch
+ cd ../..
+ cd frameworks/av
+ patch -p1 < ../../device/lenovo/A536/patches/frameworks_av.patch
+ cd ..
+ cd base
+ patch -p1 < ../../device/lenovo/A536/patches/frameworks_base.patch
+ cd ..
+ cd native
+ patch -p1 < ../../device/lenovo/A536/patches/frameworks_native.patch
+ cd ..
+ cd opt/telephony
+ patch -p1 < ../../../device/lenovo/A536/patches/frameworks_opt_telephony.patch
+ cd ../../..
+ cd external/sepolicy
+ patch -p1 < ../../device/lenovo/A536/patches/external_sepolicy.patch
+ cd ../..
+ echo Successfuly patched! Go ahead and start building!
